727e054370150e24bafc38396e24bc3c31fae960
taku10101 <taku1011ht@gmail.com>
2023-11-12 03:28:06 +0900

    handleYearAdd

--- src/hooks/useChangeMonth.tsx
+++ src/hooks/useChangeMonth.tsx
@@ -5,6 +5,13 @@ import React, { useState } from "react";
 export const ChangeMonth = () => {
   const [currentMonth, setCurrentMonth] = useState(moment().startOf("month"));
 
+  const handleNextYear = () => {
+    setCurrentMonth((prev) => prev.clone().add(1, "year"));
+  };
+  const handlePrevYear = () => {
+    setCurrentMonth((prev) => prev.clone().subtract(1, "year"));
+  };
+
   //進むボタン
   const handleNextMonth = () => {
     setCurrentMonth((prev) => prev.clone().add(1, "month"));
