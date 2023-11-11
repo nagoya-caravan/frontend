bfbffd35943d3dd293e7aec56527bd694fd43daf
Takumi <fd3s.bcnr33@gmail.com>
2023-11-11 17:18:09 +0900

    Fix mordal.jsx

--- src/components/mordal.jsx
+++ src/components/mordal.jsx
@@ -1,18 +1,18 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import Button from '@mui/material/Button';
-import HighlightOffSharpIcon from '@mui/icons-material/HighlightOffSharp';
-import '../styles/mordal.scss';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Button from "@mui/material/Button";
+import HighlightOffSharpIcon from "@mui/icons-material/HighlightOffSharp";
+import "../styles/mordal.scss";
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   pt: 2,
   px: 4,
@@ -38,14 +38,16 @@ export default function NestedModal() {
         aria-describedby="parent-modal-description"
       >
         <Box sx={{ ...style, width: 400 }}>
-            <div className='close'>
-                <HighlightOffSharpIcon onClick={handleClose}/>
-            </div>
+          <Box>
+            <HighlightOffSharpIcon
+              onClick={handleClose}
+              sx={{ position: "absolute", right: "30px" }}
+            />
+          </Box>
           <h2 id="parent-modal-title">これであってますか？</h2>
           <p id="parent-modal-description">
             あってたらボタンを入力してください。
           </p>
-
         </Box>
       </Modal>
     </div>
