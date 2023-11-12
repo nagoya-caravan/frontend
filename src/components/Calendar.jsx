57d47af1fbcdecc27248de79cbb3cadb3794864f
taku10101 <taku1011ht@gmail.com>
2023-11-12 14:11:39 +0900

    week arrayAdd

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,236 +1,75 @@
-import React, { useState, useEffect } from "react";
+import { useState } from "react";
 import moment from "moment";
-import Box from "@mui/material/Box";
-import { Button, Chip, Typography } from "@mui/material";
-import { ChangeMonth } from "../hooks/useChangeMonth";
-const CalendarComponent = () => {
-  const { currentMonth, handleNextMonth, handlePrevMonth } = ChangeMonth();
 
-  const [calendar, setCalendar] = useState([]);
+// 月初めの日付と曜日を計算するヘルパー関数
+const getMonthData = (year, month) => {
+  const firstDayOfMonth = moment([year, month]);
+  const firstDayOfWeek = firstDayOfMonth.day();
 
-  useEffect(() => {
-    const start = currentMonth.clone(); //ここで月初を取得
-    const end = moment(start).endOf("month"); //ここで月末を取得
-    const newCalendar = [];
-    console.log(start);
-    console.log(end);
-    const dayOfWeek = start.day(); //ここで月初の曜日を取得
+  let currentWeek = firstDayOfWeek; // 月初めの日の曜日
+  let daysInMonth = firstDayOfMonth.daysInMonth(); // その月の日数
+  let weeks = []; // 週の配列
+  let week = [];
 
-    const prevMonthEnd = start.clone().subtract(1, "day"); //ここで先月末を取得
-    const prevMonthStart = prevMonthEnd.clone().subtract(dayOfWeek - 1, "days"); //ここで先月初を取得
-
-    let iterDate = prevMonthStart.clone(); //ここで先月初をイテレータに代入
-    while (iterDate.isSameOrBefore(prevMonthEnd)) {
-      //ここで先月末まで繰り返す
-      newCalendar.push(
-        <Box
-          sx={{ aspectRatio: "1 / 1", border: "1px #111 solid" }}
-          key={iterDate.format("MD")}
-        >
-          <Typography
-            sx={{
-              ml: 1,
-              mt: 1,
-              fontSize: "16px",
-              color: "rgba(1, 1, 1, 0.5)",
-            }}
-          >
-            {iterDate.format("D")}
-          </Typography>
-        </Box>
-      );
-      iterDate.add(1, "days"); //ここでイテレータを1日進める
+  // 月の日数分だけループ
+  for (let day = 1; day <= daysInMonth; day++) {
+    week.push({ date: day, dayOfWeek: currentWeek });
+    if (currentWeek === 6 || day === daysInMonth) {
+      // 土曜日または月末の場合、週をリセット
+      weeks.push(week);
+      week = []; // 新しい週の配列を開始
+      currentWeek = 0; // 曜日カウンターをリセット
+    } else {
+      currentWeek++; // 曜日カウンターを進める
     }
+  }
+  return weeks; // 週の配列を返す
+};
 
-    iterDate = start.clone();
-
-    //ここから月初から月末まで繰り返す
-    while (iterDate.isSameOrBefore(end)) {
-      //ここで月末まで繰り返す
-      newCalendar.push(
-        <Box
-          sx={{
-            aspectRatio: "1 / 1",
-            border: "1px #111 solid",
-          }}
-          key={iterDate.format("MD")}
-        >
-          <Box>
-            <Typography
-              sx={{
-                ml: 1,
-                mt: 1,
-                fontSize: "16px",
-              }}
-            >
-              {iterDate.format("D")}
-            </Typography>
-          </Box>
-          <Box
-            sx={{
-              width: "100%",
-            }}
-          >
-            <Chip
-              sx={{
-                mt: 1,
-                width: "100%",
-              }}
-            />
-          </Box>
-        </Box>
-      );
-      iterDate.add(1, "days");
-    }
-
-    //ここから月末の翌日から繰り返す
-    iterDate = end.clone().add(1, "days"); //ここで月末の翌日をイテレータに代入
-    while (newCalendar.length % 7 !== 0) {
-      newCalendar.push(
-        <Box
-          sx={{ aspectRatio: "1 / 1", border: "1px #111 solid" }}
-          key={iterDate.format("MD")}
-        >
-          <Typography
-            sx={{
-              ml: 1,
-              mt: 1,
-              fontSize: "16px",
-              color: "rgba(1, 1, 1, 0.5)",
-            }}
-          >
-            {iterDate.format("D")}
-          </Typography>
-        </Box>
-      );
-      iterDate.add(1, "days");
-    }
-
-    setCalendar(newCalendar);
-  }, [currentMonth]);
+const Calendar = ({ year, month }) => {
+  const weeksOfMonth = getMonthData(year, month);
 
   return (
-    <Box
-      sx={{
-        mt: 3,
-        background: "#fff",
-      }}
-    >
-      <Box
-        sx={{
-          width: "30%",
-          textAlign: "center",
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        <Button
-          variant='outlined'
-          sx={{
-            my: 2,
-          }}
-          onClick={() => handleNextMonth()}
-        >
-          {"<"}
-        </Button>
-        {currentMonth.format("YYYY年M月")}
-        <Button
-          variant='outlined'
-          sx={{
-            my: 2,
-          }}
-          onClick={() => handlePrevMonth()}
-        >
-          {">"}
-        </Button>
-      </Box>
-      <Box
-        sx={{
-          width: "100%",
-          display: "grid",
-          gridAutoFlow: "row",
-          gridTemplateColumns: "repeat(7, 1fr)",
-          gap: 0,
-          borderBottom: "solid 1px #111",
-          borderLeft: "solid 1px #111",
-          borderRight: "solid 1px #111",
-        }}
-      >
-        <Box
-          sx={{
-            textAlign: "center",
-            color: "#111",
-            borderTop: "solid 1px #111",
-
-            borderBottom: "1px solid #111",
-          }}
-        >
-          日
-        </Box>
-        <Box
-          sx={{
-            textAlign: "center",
-            borderTop: "solid 1px #111",
-
-            borderBottom: "1px solid #111",
-          }}
-        >
-          月
-        </Box>
-        <Box
-          sx={{
-            textAlign: "center",
-            borderTop: "solid 1px #111",
-
-            borderBottom: "1px solid #111",
-          }}
-        >
-          火
-        </Box>
-        <Box
-          sx={{
-            textAlign: "center",
-            borderTop: "solid 1px #111",
-            borderBottom: "1px solid #111",
-          }}
-        >
-          水
-        </Box>
-        <Box
-          sx={{
-            textAlign: "center",
-            borderTop: "solid 1px #111",
-            borderBottom: "1px solid #111",
-          }}
-        >
-          木
-        </Box>
-        <Box
-          sx={{
-            textAlign: "center",
-            borderTop: "solid 1px #111",
-
-            borderBottom: "1px solid #111",
-          }}
-        >
-          金
-        </Box>
-        <Box
-          sx={{
-            textAlign: "center",
-            color: "#111",
-            borderTop: "solid 1px #111",
-
-            borderBottom: "1px solid #111",
-          }}
-        >
-          土
-        </Box>
-        {calendar}
-      </Box>
-    </Box>
+    <div>
+      {weeksOfMonth.map((week, index) => (
+        <div key={index} className='week-row'>
+          {week.map((day) => (
+            <div key={day.date} className='day'>
+              {day.date} ({moment().day(day.dayOfWeek).format("ddd")})
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
   );
 };
-const Calendar = React.memo(CalendarComponent, () => "Calendar");
-export default Calendar;
+
+// カレンダーコンポーネントの利用例
+const CalendarApp = () => {
+  const [currentYearMonth, setCurrentYearMonth] = useState(moment());
+
+  return (
+    <div>
+      <Calendar
+        year={currentYearMonth.year()}
+        month={currentYearMonth.month()}
+      />
+      <button
+        onClick={() =>
+          setCurrentYearMonth(currentYearMonth.clone().subtract(1, "months"))
+        }
+      >
+        Previous Month
+      </button>
+      <button
+        onClick={() =>
+          setCurrentYearMonth(currentYearMonth.clone().add(1, "months"))
+        }
+      >
+        Next Month
+      </button>
+    </div>
+  );
+};
+
+export default CalendarApp;
