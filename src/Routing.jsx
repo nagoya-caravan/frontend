2953c1f56ef3d5b795b3a18b339d615fbf2c5045
Takumi <fd3s.bcnr33@gmail.com>
2023-11-13 17:15:36 +0900

    Add SideBar2.jsx

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import TopPage from "./views/TopPage";
 import CalenderList from "./views/CalendarList";
+import SideBar2 from "./components/SideBar2";
 
 const Routing = () => {
   return (
     <Routes>
-      <Route path='/' element={<TopPage />} />
-      <Route path='/list' element={<CalenderList />} />
+      <Route path="/" element={<TopPage />} />
+      <Route path="/list" element={<CalenderList />} />
+      <Route path="/sidebar" element={<SideBar2 />}></Route>
     </Routes>
   );
 };
