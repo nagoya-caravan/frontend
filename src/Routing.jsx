6b01ef06908926bd7d1471d4bb964844fabcee69
Takumi <fd3s.bcnr33@gmail.com>
2023-11-13 22:37:38 +0900

    Fix SideBar.jsx Routing.jsx

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,14 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import TopPage from "./views/TopPage";
 import CalenderList from "./views/CalendarList";
-import SideBar2 from "./components/SideBar2";
+import SideBar from "./components/SideBar";
 
 const Routing = () => {
   return (
     <Routes>
-      <Route path="/" element={<TopPage />} />
-      <Route path="/list" element={<CalenderList />} />
-      <Route path="/sidebar" element={<SideBar2 />}></Route>
+      <Route path="/" element={<SideBar />}>
+        <Route path="/" element={<TopPage />} />
+        <Route path="/list" element={<CalenderList />} />
+      </Route>
     </Routes>
   );
 };
