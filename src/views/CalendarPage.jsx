4fce7b8059ab3c25bdc5ee6761986695a5364a3f
Takumi <fd3s.bcnr33@gmail.com>
2023-11-14 16:35:59 +0900

    Fix CalendarPage.jsx Box

--- src/views/CalendarPage.jsx
+++ src/views/CalendarPage.jsx
@@ -3,8 +3,9 @@ import Calendar from "../components/Calendar/Calendar";
 const CalendarPage = () => {
   return (
     <Box>
-      <Typography>Calendar</Typography>
-      <Calendar />
+      <Box sx={{ margin: "0 36px 36px 100px" }}>
+        <Calendar />
+      </Box>
     </Box>
   );
 };
