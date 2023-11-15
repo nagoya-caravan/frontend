593f952bbd8b6f108ec51e6702807505132d7bdc
taku10101 <taku1011ht@gmail.com>
2023-11-16 00:58:01 +0900

    デザイン

--- src/views/NotFoundPage.jsx
+++ src/views/NotFoundPage.jsx
@@ -3,15 +3,49 @@ import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
   return (
-    <Box>
-      <Typography>404 Not Found</Typography>
+    <Box
+      sx={{
+        margin: "auto",
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <Typography variant='h2'>404 Not Found</Typography>
       <Box>
-        <Typography>お探しのページは見つかりませんでした</Typography>
+        <Typography
+          variant='h4'
+          sx={{
+            mt: 2,
+            mb: 4,
+          }}
+        >
+          お探しのページは見つかりませんでした
+        </Typography>
       </Box>
-      <Box>
-        <Button>
-          <Link href='/'>トップページへ</Link>
-        </Button>
+
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          width: "30%",
+        }}
+      >
+        <Box>
+          <Link to='/'>
+            <Button variant='outlined' color='success'>
+              トップページに戻る
+            </Button>
+          </Link>
+        </Box>
+
+        <Box>
+          <Link to='/list'>
+            <Button variant='outlined' color='success'>
+              カレンダー編集
+            </Button>
+          </Link>
+        </Box>
       </Box>
     </Box>
   );
