46fc6f6f1482e2ba5f4914e7929b476c82e7a06e
taku10101 <taku1011ht@gmail.com>
2023-11-15 16:06:41 +0900

    last del

--- src/components/Calendar/WeekRow.jsx
+++ src/components/Calendar/WeekRow.jsx
@@ -1,38 +1,21 @@
 import { Box } from "@mui/material";
 const WeekRow = (props) => {
-  const { week, lastWeek } = props;
+  const { week } = props;
   return (
     <Box display={{ xs: "none", md: "flex" }}>
-      {week.map((day) =>
-        lastWeek ? (
-          <Box
-            key={day.date}
-            sx={{
-              aspectRatio: "1 / 1",
-              border: "1px #111 solid",
-              width: "100%",
-              backgroundColor: "#eee",
-              justifyContent: "flex-end",
-              alignItems: "center",
-            }}
-          >
-            {day.date}
-          </Box>
-        ) : (
-          <Box
-            key={day.date}
-            sx={{
-              aspectRatio: "1 / 1",
-              border: "1px #111 solid",
-              width: "40%",
-            }}
-          >
-            {day.date}
-          </Box>
-        )
-      )}
+      {week.map((day) => (
+        <Box
+          key={day.date}
+          sx={{
+            aspectRatio: "1 / 1",
+            border: "1px #111 solid",
+            width: "40%",
+          }}
+        >
+          {day.date}
+        </Box>
+      ))}
     </Box>
   );
 };
-
 export default WeekRow;
