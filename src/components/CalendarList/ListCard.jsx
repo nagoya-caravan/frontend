ee94beb82e35c6741dcf6a0951ea82e8a49eafcc
taku10101 <taku1011ht@gmail.com>
2023-11-18 04:04:23 +0900

    redirectAdd

--- src/components/CalendarList/ListCard.jsx
+++ src/components/CalendarList/ListCard.jsx
@@ -1,9 +1,18 @@
-import { Box, Grid, Typography, Paper, IconButton } from "@mui/material";
+import {
+  Box,
+  Grid,
+  Typography,
+  Paper,
+  IconButton,
+  Button,
+} from "@mui/material";
 import ShareIcon from "@mui/icons-material/Share";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { getCalenderList } from "../../api/apis";
 import { useState, useEffect } from "react";
 import { firebaseAuth } from "../../utils/firebaseConfig";
+import { useNavigate } from "react-router-dom";
+
 const CalendarListCard = () => {
   const firebaseUser = firebaseAuth.currentUser;
   const [calendars, setCalendars] = useState([]);
@@ -13,70 +22,100 @@ const CalendarListCard = () => {
       .then((calendars) => {
         setCalendars(calendars);
       })
+      .then(() => {
+        if (!firebaseUser) throw new Error("User is not logged in");
+      })
       .catch((err) => {
         console.log(err);
       });
   }, []);
+  const navigate = useNavigate();
+  const handleItemClick = (calendar_id) => {
+    navigate(`/calendar/${calendar_id}`);
+  };
 
   const data = calendars;
+
   return (
     <Box>
       <Grid container spacing={5}>
-        {data.map((item) => (
-          <Grid
-            item
-            key={data.calender_name}
-            sx={{
-              width: "100%",
-              margin: "30px auto 30px auto",
-            }}
-          >
-            <Paper
-              sx={{
-                margin: "0 auto 0 auto",
-                padding: "20px 100px 20px 30px",
-                width: "65%",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "space-between",
-                flexWrap: "wrap",
-              }}
-            >
-              <Box>
-                <Typography
-                  sx={{ display: "inline", opacity: 0.65, fontSize: "1.3rem" }}
+        {data.map(
+          (item) => (
+            console.log(item),
+            (
+              <Grid
+                item
+                key={data.calender_name}
+                sx={{
+                  width: "100%",
+                  margin: "30px auto 30px auto",
+                }}
+              >
+                <Paper
+                  sx={{
+                    margin: "0 auto 0 auto",
+                    padding: "20px 100px 20px 30px",
+                    width: "65%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    flexWrap: "wrap",
+                  }}
                 >
-                  カレンダー名：
-                </Typography>
-                <Typography sx={{ display: "inline", fontSize: "1.3rem" }}>
-                  {item.calender_name}
-                </Typography>
-              </Box>
+                  <Box>
+                    <Typography
+                      sx={{
+                        display: "inline",
+                        opacity: 0.65,
+                        fontSize: "1.3rem",
+                      }}
+                    >
+                      カレンダー名：
+                    </Typography>
+                    <Typography sx={{ display: "inline", fontSize: "1.3rem" }}>
+                      {item.calender_name}
+                    </Typography>
+                  </Box>
 
-              <Box>
-                <Typography
-                  sx={{ display: "inline", opacity: 0.65, fontSize: "1.3rem" }}
-                >
-                  作成者：
-                </Typography>
-                <Typography sx={{ display: "inline", fontSize: "1.3rem" }}>
-                  {firebaseUser.displayName}
-                </Typography>
-              </Box>
+                  <Box>
+                    <Typography
+                      sx={{
+                        display: "inline",
+                        opacity: 0.65,
+                        fontSize: "1.3rem",
+                      }}
+                    >
+                      作成者：
+                    </Typography>
+                    <Typography sx={{ display: "inline", fontSize: "1.3rem" }}>
+                      {firebaseUser.displayName}
+                    </Typography>
+                  </Box>
 
-              <Box>
-                <IconButton>
-                  <ShareIcon fontSize='large' />
-                </IconButton>
-              </Box>
-              <Box sx={{}}>
-                <IconButton>
-                  <DeleteIcon fontSize='large' />
-                </IconButton>
-              </Box>
-            </Paper>
-          </Grid>
-        ))}
+                  <Box>
+                    <IconButton>
+                      <ShareIcon fontSize='large' />
+                    </IconButton>
+                  </Box>
+                  <Box>
+                    <IconButton>
+                      <DeleteIcon fontSize='large' />
+                    </IconButton>
+                  </Box>
+                  <Box>
+                    <Button
+                      onClick={() => {
+                        handleItemClick(item.user_id);
+                      }}
+                    >
+                      編集
+                    </Button>
+                  </Box>
+                </Paper>
+              </Grid>
+            )
+          )
+        )}
       </Grid>
     </Box>
   );
